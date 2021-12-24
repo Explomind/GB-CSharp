8c9c45ae1de2694542c87d6369c439118c2230e0
Explomind <explomind@gmail.com>
2021-12-25 00:51:25 +0300

    IfElse added

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Please enter your name: ");
+string userName = Console.ReadLine();
+if(userName.ToLower() == "egor")
+{
+    Console.WriteLine("Greetings to You, Master Egor!");
+}
+else
+{
+    Console.WriteLine("Hello, " + userName);
+}
