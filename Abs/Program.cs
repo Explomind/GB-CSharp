bd9c9a58ccee536f66d0981500efb04e941a862e
Explomind <explomind@gmail.com>
2021-12-25 10:24:33 +0300

    Example absolute value added

---
+++ Abs/Program.cs
@@ -0,0 +1,15 @@
+﻿int value = ReadIntFromConsole("Enter integer number");
+Console.WriteLine($"Absolute value = {Absolute(value)}");
+
+int ReadIntFromConsole(string prompt)
+{
+    Console.WriteLine(prompt);
+    string result = Console.ReadLine();
+    return Convert.ToInt32(result);
+}
+
+int Absolute(int argument)
+{
+    if (argument >= 0) return argument;
+    return -argument;
+}
