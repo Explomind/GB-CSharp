626b8b531e3ef5af1e7634fe786bad815d875ed7
Explomind <explomind@gmail.com>
2022-01-25 12:43:52 +0300

    Task4 added

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 4. Найти максимальное из трех чисел
+
+int[] numArray = {5, 8, 15};
+int max = numArray[0];
+for(int i = 0; i < numArray.Length; i++)
+{
+    Console.Write($"{numArray[i]} ");
+    if(numArray[i] > max) max = numArray[i];
+}
+Console.WriteLine();
+Console.WriteLine($"Maximum value is {max}");
