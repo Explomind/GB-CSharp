c5282ae9bbbf201078c63e8d814ac25b71fb2531
Explomind <explomind@gmail.com>
2022-01-26 18:35:47 +0300

    Task 18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,21 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool[] value1 = { false, true };
+bool[] value2 = { false, true };
+for (int i = 0; i < value1.Length; i++)
+{
+    for (int j = 0; j < value2.Length; j++)
+    {
+        Console.Write($" {!(value1[i]|value2[j])} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < value1.Length; i++)
+{
+    for (int j = 0; j < value2.Length; j++)
+    {
+        Console.Write($" {!value1[i]&!value2[j]} ");
+    }
+    Console.WriteLine();
+}
