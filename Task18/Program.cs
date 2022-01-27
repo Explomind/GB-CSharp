868e38a0796c350fed9ae0d6785e497b36d0bd4f
Explomind <explomind@gmail.com>
2022-01-27 12:05:36 +0300

    Task 18 little optimization

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -6,7 +6,7 @@ for (int i = 0; i < value1.Length; i++)
 {
     for (int j = 0; j < value2.Length; j++)
     {
-        Console.Write($" {!(value1[i]|value2[j])} ");
+        Console.Write($" {!(value1[i]||value2[j])} ");
     }
     Console.WriteLine();
 }
@@ -15,7 +15,7 @@ for (int i = 0; i < value1.Length; i++)
 {
     for (int j = 0; j < value2.Length; j++)
     {
-        Console.Write($" {!value1[i]&!value2[j]} ");
+        Console.Write($" {!value1[i]&&!value2[j]} ");
     }
     Console.WriteLine();
 }
