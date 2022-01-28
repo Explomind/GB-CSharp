c6a6cf55b689e9edb107ef826daae403c1201f36
Explomind <explomind@gmail.com>
2022-01-28 18:05:27 +0300

    Tasks 27-28 added

---
+++ Tasks27-28/Program.cs
@@ -0,0 +1,34 @@
+﻿// 27. Определить количество цифр в числе
+// 28. Подсчитать сумму цифр в числе
+
+Console.WriteLine("Определить количество цифр в числе, подсчитать сумму цифр в числе");
+RepeatInput:
+Console.Write("Input number: ");
+string? strNumber = Console.ReadLine();
+try
+{
+    int number = Convert.ToInt32(strNumber);
+    int numberLength = 0;
+    if (number < 0)
+    {
+        number = -number;
+        numberLength = strNumber.Length - 1;
+    }
+    else numberLength = strNumber.Length;
+    int digitsSum = 0;
+    int[] digits = new int[numberLength];
+    for (int i = 0; i < numberLength; i++)
+    {
+        digits[i] = number % 10;
+        digitsSum=digitsSum+digits[i];
+        number = number / 10;
+        // Console.WriteLine($"{i} {digits[i]}");
+    }
+    Console.WriteLine($"The number has {numberLength} digits.");
+    Console.WriteLine($"The sum of digits is {digitsSum}");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Incorrect input!");
+    goto RepeatInput;
+}
