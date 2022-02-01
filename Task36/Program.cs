2c65796a6b6c9e88816d73661843f1317306448b
Explomind <explomind@gmail.com>
2022-02-01 13:26:49 +0300

    Task 36 upgraded

--- Task36/Program.cs
+++ Task36/Program.cs
@@ -5,26 +5,27 @@ void FillPrintArrayRandomInt(int[] intArray, int min, int max)
     Random randomInt = new Random();
     for (int i = 0; i < intArray.Length; i++)
     {
-        intArray[i] = randomInt.Next(min, max+1);
+        intArray[i] = randomInt.Next(min, max + 1);
         Console.Write($" {intArray[i]} ");
     }
     Console.WriteLine();
 }
 
-void EvenOddAmount(int[] intArray)
+(int, int) EvenOddAmount(int[] intArray)
 {
     int evenAmount = 0;
     int oddAmount = 0;
-    for(int i=0;i<intArray.Length;i++)
+    for (int i = 0; i < intArray.Length; i++)
     {
-        if(intArray[i]%2==0) evenAmount++;
+        if (intArray[i] % 2 == 0) evenAmount++;
         else oddAmount++;
     }
-    Console.WriteLine($"Amount of even elements of the array: {evenAmount}");
-    Console.WriteLine($"Amount of odd elements of the array: {oddAmount}");
+    return (evenAmount, oddAmount);
 }
 
 Console.WriteLine("Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных/четных чисел");
-int[] numbersArray = new int[25];
+int[] numbersArray = new int[15];
 FillPrintArrayRandomInt(numbersArray, 100, 999);
-EvenOddAmount(numbersArray);
+(int evenAmount, int oddAmount) = EvenOddAmount(numbersArray);
+Console.WriteLine($"Amount of even elements of the array: {evenAmount}");
+Console.WriteLine($"Amount of odd elements of the array: {oddAmount}");
