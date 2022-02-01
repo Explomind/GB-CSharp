73ed4c4efd9263bea8dce25a868bb45172056645
Explomind <explomind@gmail.com>
2022-02-01 11:03:44 +0300

    Tasks 31-32 added, FillPrintArrayRandomInt metod added

---
+++ Tasks31-32/Program.cs
@@ -0,0 +1,19 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+void FillPrintArrayRandomInt(int[] intArray, int min, int max)
+{
+    Random randomInt = new Random();
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        intArray[i] = randomInt.Next(min, max+1);
+        Console.Write($" {intArray[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Задать массив из 8 элементов и вывести их на экран");
+int[] numbers = new int[8];
+FillPrintArrayRandomInt(numbers, 1, 100);
+Console.WriteLine("Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран");
+FillPrintArrayRandomInt(numbers, 0, 1);
