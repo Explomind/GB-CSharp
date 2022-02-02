824319ffdd538b87a371ffa824cb602492ab31de
Explomind <explomind@gmail.com>
2022-02-02 23:01:28 +0300

    Task 44 upgraded

--- Task44/Program.cs
+++ Task44/Program.cs
@@ -19,9 +19,9 @@ RepeatInput:
 
 (double, double) LinearEquation(double k1, double b1, double k2, double b2)
 {
-    double X = Math.Round((b2-b1)/(k1-k2), 2, MidpointRounding.AwayFromZero);
-    double Y = Math.Round((k1*X + b1), 2, MidpointRounding.AwayFromZero);
-    return (X,Y);
+    double X = Math.Round((b2 - b1) / (k1 - k2), 2, MidpointRounding.AwayFromZero);
+    double Y = Math.Round((k1 * X + b1), 2, MidpointRounding.AwayFromZero);
+    return (X, Y);
 }
 
 Console.WriteLine("Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы");
@@ -30,5 +30,10 @@ double k1 = ReadDoubleFromConsole("Input coefficient k1: ");
 double b1 = ReadDoubleFromConsole("Input coefficient b1: ");
 double k2 = ReadDoubleFromConsole("Input coefficient k2: ");
 double b2 = ReadDoubleFromConsole("Input coefficient b2: ");
-(double X, double Y) = LinearEquation(k1, b1, k2, b2); 
-Console.WriteLine($"Point of intersection of lines: X = {X}, Y = {Y}");
+if (k1 != k2)
+{
+    (double X, double Y) = LinearEquation(k1, b1, k2, b2);
+    Console.WriteLine($"Point of intersection of lines: X = {X}, Y = {Y}");
+}
+else if (b1!=b2) Console.WriteLine("The lines are parallel.");
+else Console.WriteLine("The lines coincide.");
