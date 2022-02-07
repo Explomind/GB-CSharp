314a1ca5b657239e40675096f2c7afbba7d81350
Explomind <explomind@gmail.com>
2022-02-07 16:58:05 +0300

    Task 47, CopyIntArray added

---
+++ Task47/Program.cs
@@ -0,0 +1,38 @@
+﻿// 47. Написать программу копирования массива
+
+int[] CreateArrayRandomInt(int arraySize, int min, int max)
+{
+    int[] result = new int[arraySize];
+    Random rnd = new Random();
+    for (int i = 0; i < arraySize; i++)
+    {
+        result[i] = rnd.Next(min, max + 1);
+        // Console.Write($" {intArray[i]} ");
+    }
+    // Console.WriteLine();
+    return result;
+}
+
+void PrintIntArray(int[] array)
+{
+    foreach (var item in array) Console.Write($" {item} ");
+    Console.WriteLine();
+}
+
+int[] CopyIntArray(int[] arrayToCopy)
+{
+    int[] result = new int[arrayToCopy.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = arrayToCopy[i];
+    }
+    return result;
+}
+
+Console.WriteLine("Написать программу копирования массива");
+int[] array = CreateArrayRandomInt(10, 0, 100);
+Console.WriteLine("New random array:");
+PrintIntArray(array);
+int[] newArray = CopyIntArray(array);
+Console.WriteLine("Copy of the array:");
+PrintIntArray(newArray);
