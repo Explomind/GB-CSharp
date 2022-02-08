0357d178b63df6075a7794a8f03dc008db0e2ebf
Explomind <explomind@gmail.com>
2022-02-08 11:23:40 +0300

    Task 48, Create2DIntArray, Print2DIntArray added

---
+++ Task48/Program.cs
@@ -0,0 +1,50 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+int ReadIntFromConsole(string msg)
+{
+RepeatInput:
+    Console.Write(msg);
+    string? strResult = Console.ReadLine();
+    try
+    {
+        int result = Convert.ToInt32(strResult);
+        return result;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Incorrect input!");
+        goto RepeatInput;
+    }
+}
+
+int[,] Create2DArrayRandomInt(int qntRows, int qntColumns, int min, int max)
+{
+    int[,] result = new int[qntRows, qntColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < qntRows; i++)
+    {
+        for (int j = 0; j < qntColumns; j++)
+        {
+            result[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DIntArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Показать двумерный массив размером mxn заполненный целыми числами");
+int qntRows = ReadIntFromConsole("Input amount of rows: ");
+int qntColumns = ReadIntFromConsole("Input amount of columns: ");
+int[,] int2DArray = Create2DArrayRandomInt(qntRows, qntColumns, 0, 100);
+Print2DIntArray(int2DArray);
