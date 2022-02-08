1ca231d74436da0c91abd1213c9c071f094c8d4a
Explomind <explomind@gmail.com>
2022-02-08 12:36:41 +0300

    Task 50 added

---
+++ Task50/Program.cs
@@ -0,0 +1,64 @@
+﻿// 50. В двумерном массиве n×k заменить четные элементы на противоположные
+
+int ReadIntFromConsole(string msg)
+{
+RepeatInput:
+    Console.Write(msg);
+    string? strResult = Console.ReadLine();
+    try
+    {
+        int result = Convert.ToInt32(strResult);
+        return result;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Incorrect input!");
+        goto RepeatInput;
+    }
+}
+
+int[,] Create2DArrayRandomInt(int qntRows, int qntColumns, int min, int max)
+{
+    int[,] result = new int[qntRows, qntColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < qntRows; i++)
+    {
+        for (int j = 0; j < qntColumns; j++)
+        {
+            result[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DIntArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeEvenElements(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] % 2 == 0) array[i, j] *= -1;
+        }
+    }
+}
+
+Console.WriteLine("В двумерном массиве nxk заменить четные элементы на противоположные");
+int qntRows = ReadIntFromConsole("Input amount of rows: ");
+int qntColumns = ReadIntFromConsole("Input amount of columns: ");
+int[,] int2DArray = Create2DArrayRandomInt(qntRows, qntColumns, -100, 100);
+Print2DIntArray(int2DArray);
+ChangeEvenElements(int2DArray);
+Console.WriteLine("New array:");
+Print2DIntArray(int2DArray);
