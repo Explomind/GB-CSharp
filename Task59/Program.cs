2cfe7768db0709200e715a88facde036f141ea6e
Explomind <explomind@gmail.com>
2022-02-09 19:01:21 +0300

    Task 59 added

---
+++ Task59/Program.cs
@@ -0,0 +1,80 @@
+﻿// 59. В прямоугольной матрице найти строку с наименьшей суммой элементов.
+
+int ReadIntFromConsole(string msg)
+{
+RepeatInput:
+    Console.Write(msg);
+    string? strResult = Console.ReadLine();
+    try
+    {
+        int result = Convert.ToInt32(strResult);
+        return result;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Incorrect input!");
+        goto RepeatInput;
+    }
+}
+
+int[,] Create2DArrayRandomInt(int qntRows, int qntColumns, int min, int max)
+{
+    int[,] result = new int[qntRows, qntColumns];
+    Random rnd = new Random();
+    for (int i = 0; i < qntRows; i++)
+    {
+        for (int j = 0; j < qntColumns; j++)
+        {
+            result[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void Print2DIntArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] SumInRows(int[,] array)
+{
+    int[] result = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[i] += array[i, j];
+        }
+    }
+    return result;
+}
+
+(int, int) Minimum(int[] array)
+{
+    int min = array[0];
+    int position = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            position = i;
+        }
+    }
+    return (min, position);
+}
+
+Console.WriteLine("В прямоугольной матрице найти строку с наименьшей суммой элементов.");
+int qntRows = ReadIntFromConsole("Input amount of rows: ");
+int qntColumns = ReadIntFromConsole("Input amount of columns: ");
+int[,] int2DArray = Create2DArrayRandomInt(qntRows, qntColumns, 0, 10);
+Print2DIntArray(int2DArray);
+(int min, int rowNumber) = Minimum(SumInRows(int2DArray));
+Console.WriteLine($"Row number {rowNumber + 1} has minimum sum of elements: {min}");
