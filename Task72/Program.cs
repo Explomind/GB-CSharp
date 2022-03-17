a6ad754c86684d48b457db826e3905673f788420
Explomind <explomind@gmail.com>
2022-03-17 18:52:43 +0300

    Tasks 72-73 added

---
+++ Task72/Program.cs
@@ -0,0 +1,28 @@
+﻿// 72. Написать программу возведения числа А в целую степень B
+
+int ReadIntFromConsole(string msg)
+{
+RepeatInput:
+    Console.Write(msg);
+    string? strResult = Console.ReadLine();
+    try
+    {
+        int result = Convert.ToInt32(strResult);
+        return result;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Incorrect input!");
+        goto RepeatInput;
+    }
+}
+
+int Power(int powerBase, int exponent)
+{
+    return exponent == 0 ? 1 : powerBase * Power(powerBase, exponent - 1);
+}
+
+Console.WriteLine("Написать программу возведения числа А в целую степень B");
+int powerBase = ReadIntFromConsole("Input the base of power: ");
+int exponent = ReadIntFromConsole("Input exponent: ");
+Console.WriteLine($"{powerBase}^{exponent} = {Power(powerBase, exponent)}");
---
+++ Task73/Program.cs
@@ -0,0 +1,31 @@
+﻿// 73. Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+int ReadIntFromConsole(string msg)
+{
+RepeatInput:
+    Console.Write(msg);
+    string? strResult = Console.ReadLine();
+    try
+    {
+        int result = Convert.ToInt32(strResult);
+        return result;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Incorrect input!");
+        goto RepeatInput;
+    }
+}
+
+string NumberSeries(int number1, int number2, int N)
+{
+    if (N == 1) return String.Empty;
+    else return $"{number2} " + NumberSeries(number2, number1 + number2, N - 1);
+}
+
+Console.WriteLine("Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих");
+int number1 = ReadIntFromConsole("Input the first number of the series: ");
+int number2 = ReadIntFromConsole("Input the second number of the series: ");
+int amount = ReadIntFromConsole("Input amount of numbers: ");
+Console.WriteLine($"{number1} " + NumberSeries(number1, number2, amount));
