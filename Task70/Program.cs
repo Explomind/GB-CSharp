52c4618e6dbd585532e59aa0f4cc4f6f53fc3557
Explomind <explomind@gmail.com>
2022-03-17 14:15:42 +0300

    Task 70 added

---
+++ Task70/Program.cs
@@ -0,0 +1,27 @@
+﻿// 70. Найти сумму цифр числа
+
+int ReadIntFromConsole(string msg)
+{
+RepeatInput:
+    Console.Write(msg);
+    string? strResult = Console.ReadLine();
+    try
+    {
+        int result = Convert.ToInt32(strResult);
+        return result;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Incorrect input!");
+        goto RepeatInput;
+    }
+}
+
+int SumDigits(int number)
+{
+    return number == 0 ? 0 : number % 10 + SumDigits(number / 10);
+}
+
+Console.WriteLine("Найти сумму цифр числа");
+int number = ReadIntFromConsole("Input number: ");
+Console.WriteLine($"Sum of digits: {SumDigits(number)}");
