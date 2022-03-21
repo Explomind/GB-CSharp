d445646b9e92517a9e9149dcb5d41d09cf36444c
Explomind <explomind@gmail.com>
2022-03-21 17:18:51 +0300

    Task 74 (machine alphabet) added

---
+++ Task74/Program.cs
@@ -0,0 +1,19 @@
+﻿// 74. В некотором машинном алфавите имеются четыре буквы «а», «b», «с» и «d». 
+// Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита
+
+int n = 1;
+
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++}\t{ new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+
+FindWords("abcd", new char[3]);
